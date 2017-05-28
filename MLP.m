e79a58ce4ec4d4548eb6230fc2f2497693fe9513
butyoucancallmecharlie@gmail.com <butyoucancallmecharlie@gmail.com>
2017-05-27 22:35:57 -0500

    Error de iteraciÃn
    Calcula el error de iteraciÃn como el promedio de la multiplicaciÃn de
    (t-a)transpuesta por (t-a). Creo que hacen eso para asegurar que el error
    sea de 1x1.
    
    Si se alcanza el error mÃnimo antes de que se haga el nÃmero tope de
    iteraciones, finaliza el ciclo y dice que es una aproximaciÃn exitosa.

--- MLP.m
+++ MLP.m
@@ -1,8 +1,9 @@
 function MLP
 	nombreArchivoTabla = input('Escriba el archivo con los datos del conjunto de entrenamiento: ', 's');
 	nombreArchivoTargets =  input('Escriba el archivo con los targets: ', 's');
-	itmax = input('Numero de iteraciones maximas: ');
+	itmax = input('Número de iteraciones máximas: ');
 	alpha = input('Factor de aprendizaje: ');
+    error = input('Error de iteración: ');  
 	tabla = load(nombreArchivoTabla);
 	t= load(nombreArchivoTargets);
 	S1 = 2
@@ -23,7 +24,10 @@ function MLP
 	tabla = tabla.';
 	t = t.';
 	tic()
+    
+    terminar_error = 0 ;
 	while it < itmax
+        err_it = 0 ;
 		for m = 1:Q
 			%%Propagacion hacia delante del m-esimo elemento
 			a1 = logsig(W1*(tabla(:, m)) + b1);
@@ -38,18 +42,33 @@ function MLP
 			b2 = b2 - alpha*s2;
 			W1 = W1 - alpha*s1*(tabla(:, m).');
 			b1 = b1 - alpha*s1;
+            %%Calcular error
+            err_it = err_it + (transpose(e)*e) ;
 		end
 		it = it+1;
+        %%Verificar condición de terminación por error.
+        err_it = err_it / Q ;
+        if error > err_it 
+            terminar_error = 1 ;
+            break;
+        end 
 	end
 	disp(toc())
-	a = zeros(1, Q)
+	a = zeros(1, Q);
 	for m=1:Q
 		a1 = logsig(W1*(tabla(:, m)) + b1);
 		a2 = purelin(W2*a1 + b2);
 		a(m) = a2;
 	end
 	disp(a);
-
+    
+    if terminar_error > 0
+        disp('Aproximación exitosa.');
+    else
+        disp('Aproximación no exitosa');
+    end
+    disp(err_it);
+    
 	figure
 	hold on
 	plot(tabla, t, '* ');
